c0b499d6bb820390068d711f247396da4cc82262
Jason Decastro <jasonrdecastro@gmail.com>
2016-08-12 11:24:21 -0400

    finished sinatra nested forms pirates lab

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
+  attr_accessor :first_ship, :second_ship
+
+  @@all = []
+
+  def initialize(params)
+    @first_ship = params[:pirate][:ships][0]
+    @second_ship = params[:pirate][:ships][1]
+
+    @@all << @first_ship
+    @@all << @second_ship
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all = []
+  end
 end
